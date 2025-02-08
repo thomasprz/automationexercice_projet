5df29b537ac3eb772873e1003d466818b6701bc0
Thomas <przegralek.thomas@gmail.com>
2025-02-08 13:24:15 +0100

    Refactor: Update header component

--- components/header.component.ts
+++ components/header.component.ts
@@ -1,6 +1,7 @@
 import {Page, Locator, expect} from '@playwright/test'
 import { HomePage } from '../pages/home.page'
 import { LoginPage } from '../pages/login.page'
+import { DeleteAccountPage } from '../pages/delete-account.page'
 
 export class HeaderComponent{
     readonly page : Page
@@ -12,6 +13,7 @@ export class HeaderComponent{
     readonly apiTesting : Locator
     readonly videoTutorials : Locator
     readonly contactUs : Locator
+    readonly deleteAccount : Locator
 
     constructor(page:Page){
         this.page = page
@@ -22,7 +24,8 @@ export class HeaderComponent{
         this.testCases = page.getByRole('link', {name:' Test Cases'}) 
         this.apiTesting = page.getByRole('link', {name:' API Testing'})
         this.videoTutorials = page.getByRole('link', {name:' Video Tutorials'})
-        this.contactUs = page.getByRole('link', {name:' Contact us'})
+        this.contactUs = page.getByRole('link', {name:' Contact us'}) // On utilie 'link', car <a href="..."> sont des liens directs
+        this.deleteAccount = page.getByRole('link', {name:' Delete Account'})
     }
 
     async openHome(){
@@ -33,5 +36,16 @@ export class HeaderComponent{
     async openSignupLogin(){
         await this.signupLogin.click()
         return new LoginPage(this.page)
+    }
+
+    async openDeleteAccount(){
+        await this.deleteAccount.click()
+        return new DeleteAccountPage(this.page)
+    }
+
+    async expectLoggedUser(username){
+        // On utilise hasText pour rechercher un texte spécifique dans un élément, et listitem car l'élément "Logged in as {username}" est dans un <li> (élément de liste), pas un lien direct <a>.
+        const loggedInLocator = this.page.getByRole('listitem').filter({ hasText: `Logged in as ${username}` });
+        await expect(loggedInLocator).toBeVisible();
     }
 }
