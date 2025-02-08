4c77949182850d3ad54dc612531a7fe5606fcd7a
Thomas <przegralek.thomas@gmail.com>
2025-02-08 13:24:52 +0100

    Test: Update test cases

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '../fixtures/base.fixture';
-import { createSignupUser } from '../factories/signup.factory';
+import { accountInformation, addressInformation, createSignupUser } from '../factories/signup.factory';
 
 test.describe('Test Cases automationexercice.com', () => {
   
@@ -9,13 +9,26 @@ test.describe('Test Cases automationexercice.com', () => {
         await home.expectHomePage();
     });
 
-    test('Test Case 1: Register User', async ({ home, login, header }) => {
+    test('Test Case 1: Register User', async ({ home, login, header, signup, accountCreated, deleteAccount }) => {
         //Arrange
         const userBaseData = createSignupUser()
+        const userAccountInformation = accountInformation() //On récupère le résultat de la fonction enterAccountInformation() qui utilise Faker et on l'affecte à une variable.
+        const userAddressInformation = addressInformation()
 
         //Act
         await header.openSignupLogin() 
         await login.expectLoginPage()
         await login.fillUserSignup(userBaseData)
+        await signup.expectSignupPage()
+        await signup.expectAccountInformation(userBaseData)
+        await signup.fillAccountInformation(userAccountInformation)
+        await signup.fillAddressInformation(userAddressInformation)
+        await accountCreated.expectAccountCreated()
+        await accountCreated.clickContinue()
+        await header.expectLoggedUser(userBaseData.name)
+        await header.openDeleteAccount()
+
+        //Assert
+        await deleteAccount.expectDeleteAccount()
     });
 });
