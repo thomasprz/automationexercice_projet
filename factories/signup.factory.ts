0e770b4be6e40a571e50726bdb07c33acccfa214
Thomas <przegralek.thomas@gmail.com>
2025-02-08 13:24:26 +0100

    Fix: Modify signup factory

--- factories/signup.factory.ts
+++ factories/signup.factory.ts
@@ -6,3 +6,27 @@ export function createSignupUser() {
         email: faker.internet.email(),
     };
 }
+
+export function accountInformation(){
+    return {
+        password: faker.internet.password(),
+        days : faker.number.int({ min: 1, max: 31 }).toString(),
+        months: faker.number.int({ min: 1, max: 12 }).toString(),
+        years : faker.number.int({ min: 1900, max: 2021 }).toString(),
+    };
+}
+
+export function addressInformation(){
+    return{
+        firstname: faker.person.firstName(),
+        lastname : faker.person.lastName(),
+        company : faker.person.jobTitle(),
+        address : faker.location.streetAddress(),
+        address2 : faker.location.secondaryAddress(),
+        country : 'Australia',
+        state : faker.location.state(),
+        city : faker.location.city(),
+        zipcode : faker.location.zipCode(),
+        mobile : faker.phone.number(),
+    };
+}
