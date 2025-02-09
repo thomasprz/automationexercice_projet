ae6ff64e2ecf0f75da3046e5671a62fed142ac81
Thomas <przegralek.thomas@gmail.com>
2025-02-09 15:20:13 +0100

    Ajout de nouveaux tests dans tests.spec.ts pour la gestion de la connexion avec email valide/incorrect

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '../fixtures/base.fixture';
 import { accountInformation, addressInformation, createSignupUser } from '../factories/signup.factory';
+import { createAccountAPI } from '../factories/api.factory';
+import { createFakeLoginUser } from '../factories/login.factory';
 
 test.describe('Test Cases automationexercice.com', () => {
   
@@ -18,6 +20,7 @@ test.describe('Test Cases automationexercice.com', () => {
         //Act
         await header.openSignupLogin() 
         await login.expectLoginPage()
+        await expect(login.headerSignup).toBeVisible()
         await login.fillUserSignup(userBaseData)
         await signup.expectSignupPage()
         await signup.expectAccountInformation(userBaseData)
@@ -31,4 +34,36 @@ test.describe('Test Cases automationexercice.com', () => {
         //Assert
         await deleteAccount.expectDeleteAccount()
     });
+
+    test('Test Case 2: Login User with correct email and password', async ({header, login, deleteAccount, api, apiR}) => {
+        //Arrange
+        const createAccountAPIData = createAccountAPI()
+        //Act
+        await header.openSignupLogin()
+        await login.expectLoginPage()
+        await expect(login.headerLogin).toBeVisible()
+
+        const response = await api.createUser(createAccountAPIData);
+        const responseBody = await response.json();
+        apiR.checkResponseCode(responseBody, 201);
+        apiR.checkResponseMessage(responseBody, 'User created!');
+
+        await login.fillLoginAccount(createAccountAPIData)
+        await header.expectLoggedUser(createAccountAPIData.name)
+        await header.openDeleteAccount()
+
+        //Assert
+        await deleteAccount.expectDeleteAccount()
+    })
+
+
+    test('Test Case 3: Login User with incorrect email and password', async ({header, login}) => {
+        //Arrange
+        const userLoginData = createFakeLoginUser()
+        //Act
+        await header.openSignupLogin()
+        await login.expectLoginPage()
+        await login.fillLoginAccount(userLoginData)
+        await expect(login.paragraphLoginIncorrectData).toBeVisible()
+    })
 });
