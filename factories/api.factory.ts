395ee2f1684704b591746bca8253e79ab0c02ea3
Thomas <przegralek.thomas@gmail.com>
2025-02-09 15:20:54 +0100

    Création de api.factory.ts : génération dynamique des utilisateurs pour tests API

---
+++ factories/api.factory.ts
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker";
+
+export function createAccountAPI(){
+    return {
+        name: faker.person.firstName(),
+        email: faker.internet.email(), 
+        password: faker.internet.password(), 
+        title : 'Mr',
+        birth_date : faker.number.int({ min: 1, max: 31 }).toString(),
+        birth_month : faker.number.int({ min: 1, max: 12 }).toString(),
+        birth_year : faker.number.int({ min: 1900, max: 2021 }).toString(),
+        firstname : faker.person.firstName(),
+        lastname : faker.person.lastName(),
+        company : faker.person.jobTitle(),
+        address1 : faker.location.streetAddress(),
+        address2 : faker.location.secondaryAddress(),
+        country : 'Australia',
+        zipcode : faker.location.zipCode(),
+        state : faker.location.state(),
+        city : faker.location.city(),
+        mobile_number : faker.phone.number(),
+    }
+}
+
