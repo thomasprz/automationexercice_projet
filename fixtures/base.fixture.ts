ea36094bbce043c21a5017bef86a93c244617742
Thomas <przegralek.thomas@gmail.com>
2025-02-09 15:17:48 +0100

    Amélioration de base.fixture.ts : gestion des fixtures

--- fixtures/base.fixture.ts
+++ fixtures/base.fixture.ts
@@ -1,7 +1,8 @@
 import { pages } from "./page.fixture";
 import { components } from "./components.fixture";
+import { api } from "./api.fixture";
 import { mergeTests } from "@playwright/test";
 
-export const test = mergeTests(pages, components)
+export const test = mergeTests(pages, components, api)
 
 export{expect, Locator} from '@playwright/test'
