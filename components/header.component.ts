25ea7477b1411ed4d0b49ab60fc2edc460caae5f
Thomas <przegralek.thomas@gmail.com>
2025-02-10 22:35:38 +0100

    Mise à jour du header component

--- components/header.component.ts
+++ components/header.component.ts
@@ -2,6 +2,9 @@ import {Page, Locator, expect} from '@playwright/test'
 import { HomePage } from '../pages/home.page'
 import { LoginPage } from '../pages/login.page'
 import { DeleteAccountPage } from '../pages/delete-account.page'
+import { ContactPage } from '../pages/contact.page'
+import { TestCasesPages } from '../pages/test-cases.page'
+import { ProductsPage } from '../pages/products.page'
 
 export class HeaderComponent{
     readonly page : Page
@@ -14,6 +17,7 @@ export class HeaderComponent{
     readonly videoTutorials : Locator
     readonly contactUs : Locator
     readonly deleteAccount : Locator
+    readonly logout : Locator
 
     constructor(page:Page){
         this.page = page
@@ -21,11 +25,12 @@ export class HeaderComponent{
         this.products = page.getByRole('link', {name:' Products'})
         this.cart = page.getByRole('link', {name:' Cart'})
         this.signupLogin = page.getByRole('link', {name:' Signup / Login'})
-        this.testCases = page.getByRole('link', {name:' Test Cases'}) 
+        this.testCases = page.getByRole('link', { name: 'Test Cases', exact: true });
         this.apiTesting = page.getByRole('link', {name:' API Testing'})
         this.videoTutorials = page.getByRole('link', {name:' Video Tutorials'})
         this.contactUs = page.getByRole('link', {name:' Contact us'}) // On utilie 'link', car <a href="..."> sont des liens directs
         this.deleteAccount = page.getByRole('link', {name:' Delete Account'})
+        this.logout = page.getByRole('link', {name:' Logout'})
     }
 
     async openHome(){
@@ -38,9 +43,29 @@ export class HeaderComponent{
         return new LoginPage(this.page)
     }
 
-    async openDeleteAccount(){
+    async clickDeleteAccount(){
         await this.deleteAccount.click()
         return new DeleteAccountPage(this.page)
+    }
+
+    async clickLogout(){
+        await this.logout.click()
+        return new LoginPage(this.page)
+    }
+
+    async openContactUs(){
+        await this.contactUs.click()
+        return new ContactPage(this.page)
+    }
+
+    async openTestCases(){
+        await this.testCases.click()
+        return new TestCasesPages(this.page)
+    }
+
+    async openProducts(){
+        await this.products.click()
+        return new ProductsPage(this.page)
     }
 
     async expectLoggedUser(username){
