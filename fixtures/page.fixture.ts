a6a7b0e87aca09be9bf01c3d9c67d39e29915ee1
Thomas <przegralek.thomas@gmail.com>
2025-02-10 22:35:52 +0100

    Mise à jour de la fixture des pages

--- fixtures/page.fixture.ts
+++ fixtures/page.fixture.ts
@@ -5,6 +5,10 @@ import { SignupPage } from "../pages/signup.page";
 import { AccountCreatedPage } from "../pages/account-created.page";
 import { DeleteAccountPage } from "../pages/delete-account.page";
 import {test as pagesTest} from '@playwright/test' // On renomme test de Playwright pour éviter toute confusion avec d'autres variables.
+import { ContactPage } from "../pages/contact.page";
+import { TestCasesPages } from "../pages/test-cases.page";
+import { ProductsPage } from "../pages/products.page";
+import { ProductDetailsPage } from "../pages/product-details.page";
 
 interface Pages {
     base : BasePage
@@ -13,6 +17,10 @@ interface Pages {
     signup : SignupPage
     accountCreated : AccountCreatedPage
     deleteAccount : DeleteAccountPage
+    contact : ContactPage
+    testcases : TestCasesPages
+    products : ProductsPage
+    productDetails : ProductDetailsPage
 }
 
 export const pages = pagesTest.extend<Pages>({ //On étend la fonctionnalité de Playwright en ajoutant des objets Pages.
@@ -33,5 +41,17 @@ export const pages = pagesTest.extend<Pages>({ //On étend la fonctionnalité de
     },
     deleteAccount: async ({page}, use) => {
         await use(new DeleteAccountPage(page))
+    },
+    contact: async({page}, use) => {
+        await use(new ContactPage(page))
+    },
+    testcases: async({page}, use) => {
+        await use(new TestCasesPages(page))
+    },
+    products: async({page}, use) => {
+        await use(new ProductsPage(page))
+    },
+    productDetails: async({page}, use) => {
+        await use(new ProductDetailsPage(page))
     }
 })
