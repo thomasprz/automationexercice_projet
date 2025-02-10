f75f0c024e2f4c75ed133f68afab905d6010b4e4
Thomas <przegralek.thomas@gmail.com>
2025-02-10 22:36:27 +0100

    Mise à jour des tests

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '../fixtures/base.fixture';
 import { accountInformation, addressInformation, createSignupUser } from '../factories/signup.factory';
 import { createAccountAPI } from '../factories/api.factory';
 import { createFakeLoginUser } from '../factories/login.factory';
+import { createContactUsForm } from '../factories/contact.factory'
 
 test.describe('Test Cases automationexercice.com', () => {
   
@@ -29,7 +30,7 @@ test.describe('Test Cases automationexercice.com', () => {
         await accountCreated.expectAccountCreated()
         await accountCreated.clickContinue()
         await header.expectLoggedUser(userBaseData.name)
-        await header.openDeleteAccount()
+        await header.clickDeleteAccount()
 
         //Assert
         await deleteAccount.expectDeleteAccount()
@@ -50,7 +51,7 @@ test.describe('Test Cases automationexercice.com', () => {
 
         await login.fillLoginAccount(createAccountAPIData)
         await header.expectLoggedUser(createAccountAPIData.name)
-        await header.openDeleteAccount()
+        await header.clickDeleteAccount()
 
         //Assert
         await deleteAccount.expectDeleteAccount()
@@ -63,7 +64,74 @@ test.describe('Test Cases automationexercice.com', () => {
         //Act
         await header.openSignupLogin()
         await login.expectLoginPage()
+        await expect(login.headerLogin).toBeVisible()
         await login.fillLoginAccount(userLoginData)
         await expect(login.paragraphLoginIncorrectData).toBeVisible()
+    })
+
+    test('Test Case 4: Logout User', async ({header, login}) => {
+        //Arrange
+        const userLoginData = {name: process.env.USERNAME , email:process.env.EMAIL, password: process.env.PASSWORD}
+
+        //Act
+        await header.openSignupLogin()
+        await login.expectLoginPage()
+        await expect(login.headerLogin).toBeVisible()
+        await login.fillLoginAccount(userLoginData)
+        await header.expectLoggedUser(userLoginData.name)
+        await header.clickLogout()
+        await login.expectLoginPage()
+    })
+
+    test('Test Case 5: Register User with existing email', async ({header, login}) => {
+        //Arrange 
+        const userBaseData = {name: process.env.USERNAME, password: process.env.PASSWORD , email:process.env.EMAIL }
+        //Act
+        await header.openSignupLogin()
+        await login.expectLoginPage()
+        await expect(login.headerSignup).toBeVisible()
+        await login.fillUserSignup(userBaseData)
+        await expect(login.paragraphSignupExistData).toBeVisible()
+    })
+
+    test('Test Case 6: Contact Us Form', async ({header, contact, home}) => {
+        //Arrange
+        const contactUsFormData = createContactUsForm()
+
+        //Act
+        await header.openContactUs()
+        await expect(contact.getInTouchTitle).toBeVisible()
+        await contact.fillContact(contactUsFormData)
+        await contact.catchDialog()
+        await contact.buttonAcceptDialog.click()
+        await expect(contact.alertMessage).toBeVisible()
+        await contact.clickHomeButton()
+        await home.expectHomePage()
+
+    })
+
+    test('Test Case 7: Verify Test Cases Page', async ({header, home, testcases}) => {
+        await header.openTestCases()
+        await testcases.expectTestCasesPage()
+
+    })
+
+    test('Test Case 8: Verify All Products and product detail page', async ({header, products, productDetails}) => {
+        //Arrange
+        const detailsData = {
+            name: 'Blue Top',
+            category: 'Women > Tops',
+            price: '500',
+            availability: 'In Stock',
+            condition: 'New',
+            brand: 'Polo',
+          };
+
+        //Act
+        await header.openProducts()
+        await products.expectProductsPage()
+        await products.openFirstViewProduct()
+        await productDetails.expectProductDetailsPage()
+        await productDetails.expectProductDetails(detailsData)
     })
 });
