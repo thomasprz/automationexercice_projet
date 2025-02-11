12bdeb2be8d233aa677fe79b794f38665441f444
Thomas <przegralek.thomas@gmail.com>
2025-02-11 22:19:48 +0100

    New: Ajout du composant footer.component.ts

---
+++ components/footer.component.ts
@@ -0,0 +1,26 @@
+import {Page, Locator, expect} from'@playwright/test'
+import { BasePage } from '../pages/base.page'
+
+export class FooterComponent{
+    readonly page : Page
+    readonly subscriptionHeader : Locator
+    readonly subscriptionEmail : Locator
+    readonly subscribeButton : Locator
+    readonly copyright : Locator
+    readonly alertSuccessSubs : Locator
+
+    constructor(page:Page){
+        this.page = page
+        this.subscriptionHeader = page.getByRole('heading', {name:'Subscription', exact:true})
+        this.subscriptionEmail = page.getByRole('textbox', { name: 'Your email address' });
+        this.subscribeButton = page.locator('#subscribe')
+        this.copyright = page.locator('.pull-left')
+        this.alertSuccessSubs = page.locator('.alert-success')
+    }
+
+    async fillSubscription(email){
+        await this.subscriptionEmail.fill(email)
+        await this.subscribeButton.click()
+    }
+}
+
