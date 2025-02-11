e3c4a451322b32a33dd089f022cce0f4eff58d2e
Thomas <przegralek.thomas@gmail.com>
2025-02-11 22:18:12 +0100

    Modifications des fixtures/components.fixture.ts

--- fixtures/components.fixture.ts
+++ fixtures/components.fixture.ts
@@ -1,13 +1,18 @@
 import { BasePage } from "../pages/base.page";
 import { HeaderComponent } from "../components/header.component"; 
 import {test as componentsTest} from '@playwright/test'
+import { FooterComponent } from "../components/footer.component";
 
 interface Components {
     header : HeaderComponent
+    footer : FooterComponent
 }
 
 export const components = componentsTest.extend<Components>({
     header: async({page}, use) => {
         await use(new HeaderComponent(page))
     },
+    footer: async({page}, use) => {
+        await use(new FooterComponent(page))
+    }
 })
