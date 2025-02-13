f7918618dc5859698e23fab88985ef771042c076
Thomas <przegralek.thomas@gmail.com>
2025-02-13 21:42:03 +0100

    Modification de la page d'accueil

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -3,15 +3,30 @@ import { BasePage } from "./base.page";
 
 export class HomePage extends BasePage {
         readonly title : Locator
+        readonly linkViewProduct : Locator
+        readonly addToCart : Locator
+        readonly continueShoppingButton : Locator
 
     constructor(page:Page){
         super(page)
         this.title = page.getByAltText('Website for automation practice')
-
+        this.linkViewProduct = page.getByRole('link', {name: 'View Product'})
+        this.addToCart = page.locator('.add-to-cart')
+        this.continueShoppingButton = page.getByRole('button', {name:'Continue Shopping'})
     }
+
     //Assertions
     async expectHomePage(){
         await expect.soft(this.page).toHaveURL('/') //Avec soft() (le test continue même si une assertion échoue.)        
         await expect(this.title).toBeVisible()
     }
+
+    async clickOnProduct(index){
+        await this.linkViewProduct.nth(index).click()
+    }
+
+    async addProductNumberAndContinue(index) {
+        await this.addToCart.nth(index).click();
+        await this.continueShoppingButton.click();
+      }
 }
