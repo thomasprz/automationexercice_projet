436c11732e41b1dcb098c8eeec8fab1a6665aa21
Thomas <przegralek.thomas@gmail.com>
2025-02-14 21:25:17 +0100

    Modification du fichier fixtures/components.fixture.ts

--- fixtures/components.fixture.ts
+++ fixtures/components.fixture.ts
@@ -2,10 +2,12 @@ import { BasePage } from "../pages/base.page";
 import { HeaderComponent } from "../components/header.component"; 
 import {test as componentsTest} from '@playwright/test'
 import { FooterComponent } from "../components/footer.component";
+import { LeftSidebarComponent } from "../components/sidebar.component";
 
 interface Components {
     header : HeaderComponent
     footer : FooterComponent
+    leftSidebar : LeftSidebarComponent
 }
 
 export const components = componentsTest.extend<Components>({
@@ -14,5 +16,8 @@ export const components = componentsTest.extend<Components>({
     },
     footer: async({page}, use) => {
         await use(new FooterComponent(page))
-    }
+    },
+    leftSidebar: async({page}, use) => {
+        await use(new LeftSidebarComponent(page))
+    },
 })
