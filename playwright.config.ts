a31e618f3b00ad9959f52e252f8cf342704b799b
Thomas <przegralek.thomas@gmail.com>
2025-02-16 11:27:30 +0100

    Mise à jour de la configuration des workers dans le fichier de config Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined, // Plus il y a de workers, plus les tests peuvent s'exécuter simultanément.
   reporter: 'html',
   use: {
    baseURL: 'https://automationexercise.com/',
