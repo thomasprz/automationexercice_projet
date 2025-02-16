0dc5d2ac765b6e96f1ff63a1f0083b1c5939c267
Thomas <przegralek.thomas@gmail.com>
2025-02-16 11:26:26 +0100

    Mise à jour de la page d'accueil

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -3,31 +3,43 @@ import { BasePage } from "./base.page";
 import { ProductsPage } from "./products.page";
 import { LeftSidebarComponent } from "../components/sidebar.component";
 import { HeaderComponent } from "../components/header.component";
+import { CartPage } from "./cart.page";
+import { FooterComponent } from "../components/footer.component";
 
 export class HomePage extends BasePage {
         readonly title : Locator
+        readonly headerFullFledged : Locator
         readonly linkViewProduct : Locator
         readonly addToCart : Locator
         readonly continueShoppingButton : Locator
+        readonly headerRecommendedItems : Locator
+        readonly linkAddToCartFromRecommendedItems : Locator
+        readonly viewCartButton : Locator
+        readonly headerSubscription : Locator
         readonly products : ProductsPage
+        readonly footer : FooterComponent
         readonly leftSidebar : LeftSidebarComponent
         readonly header : HeaderComponent
 
     constructor(page:Page){
         super(page)
         this.title = page.getByAltText('Website for automation practice')
+        this.headerFullFledged = page.getByRole('heading', { name: 'Full-Fledged practice website for Automation Engineers' });
         this.linkViewProduct = page.getByRole('link', {name: 'View Product'})
         this.addToCart = page.locator('.add-to-cart')
         this.continueShoppingButton = page.getByRole('button', {name:'Continue Shopping'})
+        this.headerRecommendedItems = page.getByRole('heading', {name:'recommended items'})
+        this.linkAddToCartFromRecommendedItems = page.locator('#recommended-item-carousel').locator('.add-to-cart');
+        this.viewCartButton = page.getByRole('link', {name:'View Cart'})
         this.products = new ProductsPage(page);
+        this.footer = new FooterComponent(page)
         this.leftSidebar = new LeftSidebarComponent(page);
         this.header = new HeaderComponent(page)
 
     }
 
-    //Assertions
     async expectHomePage(){
-        await expect.soft(this.page).toHaveURL('/') //Avec soft() (le test continue même si une assertion échoue.)        
+        await expect.soft(this.page).toHaveURL('/') 
         await expect(this.title).toBeVisible()
     }
 
@@ -39,4 +51,11 @@ export class HomePage extends BasePage {
         await this.addToCart.nth(index).click();
         await this.continueShoppingButton.click();
       }
+
+    async addFromRecommendedItemsAndViewCart() {
+        await this.linkAddToCartFromRecommendedItems.last().click();
+        await this.viewCartButton.click();
+        return new CartPage(this.page);
+      }
+    
 }
