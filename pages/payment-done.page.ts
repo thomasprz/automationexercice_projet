5e3a614e03361e622e7a768d25cd8828139f97c9
Thomas <przegralek.thomas@gmail.com>
2025-02-16 11:26:44 +0100

    Mise à jour de la page de confirmation de paiement

--- pages/payment-done.page.ts
+++ pages/payment-done.page.ts
@@ -1,19 +1,33 @@
-import {Locator, Page, expect} from '@playwright/test'
+import { Locator, Page} from '@playwright/test'
 import { BasePage } from './base.page'
+import { HomePage } from './home.page'
 
 export class PaymentDonePage extends BasePage {
-    readonly orderPlacedTitle : Locator
-    readonly continueButton : Locator
-    readonly orderConfirmed : Locator
-    readonly downloadInvoiceButton : Locator
+    readonly buttonContinue : Locator
+    readonly buttonDownloadInvoice : Locator
 
 
     constructor(page:Page){
         super(page)
-        this.orderPlacedTitle = page.locator('[data-qa="order-placed"]')
-        this.orderConfirmed = page.locator('.col-sm-9 > p')
-        this.continueButton = page.getByRole('link', {name:'Continue'})
-        this.downloadInvoiceButton = page.getByRole('link', {name:'Download Invoice'})
+        this.buttonContinue = page.getByRole('link', {name:'Continue'})
+        this.buttonDownloadInvoice = page.getByRole('link', {name:'Download Invoice'})
     }
 
+    async clickContinue() {
+        await this.buttonContinue.click();
+        return new HomePage(this.page);
+      }
+
+    async downloadInvoice() {
+        const downloadPromise = this.page.waitForEvent('download'); //Retourne une Promise, c'est un objet qui représente une opération asynchrone dont le résultat n'est pas encore disponible immédiatement, mais qui le sera à l'avenir.
+
+        await this.buttonDownloadInvoice.click();
+        const download = await downloadPromise; //Permet d’attendre que l’événement 'download' se déclenche.
+        if (download) {
+          await download.saveAs('./e2e/download/invoice/invoice.txt'); //download.saveAs(path) enregistre le fichier à un emplacement spécifique.
+          console.log('File downloaded successfully.');
+        } else {
+          console.log('File download failed.');
+        }
+      }
 }
